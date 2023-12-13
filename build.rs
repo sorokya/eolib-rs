04c764082d1363a59964f692d269431b776e35eb
Richard Leek <richard@richardleek.com>
2023-12-13 10:28:40 -0500

    XML parsing working

--- build.rs
+++ build.rs
@@ -1,12 +1,166 @@
 use glob::glob;
-use std::fs::File;
-use std::io::BufReader;
-use xml::reader::{EventReader, XmlEvent};
+use serde::Deserialize;
+use std::{fs::File, io::Read};
 
 macro_rules! p {
     ($($tokens: tt)*) => {
         println!("cargo:warning={}", format!($($tokens)*))
     }
+}
+
+#[derive(Debug, Deserialize)]
+struct Protocol {
+    #[serde(rename = "$value", default)]
+    pub elements: Vec<Element>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum Element {
+    Enum(Enum),
+    Struct(Struct),
+    Packet(Packet),
+}
+
+#[derive(Debug, Deserialize)]
+struct Enum {
+    #[serde(rename = "@name")]
+    pub name: String,
+    #[serde(rename = "@type")]
+    pub data_type: String,
+    #[serde(rename = "$value", default)]
+    pub elements: Vec<EnumElement>,
+}
+
+#[derive(Debug, Deserialize)]
+enum EnumElement {
+    #[serde(rename = "comment")]
+    Comment(String),
+    #[serde(rename = "value")]
+    Value(EnumValue),
+}
+
+#[derive(Debug, Deserialize)]
+struct EnumValue {
+    #[serde(rename = "@name")]
+    pub name: String,
+    #[serde(rename = "comment")]
+    pub comment: Option<String>,
+    #[serde(rename = "$text")]
+    pub value: i32,
+}
+
+#[derive(Debug, Deserialize)]
+struct Struct {
+    #[serde(rename = "@name")]
+    pub name: String,
+    #[serde(rename = "$value", default)]
+    pub elements: Vec<StructElement>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "lowercase")]
+enum StructElement {
+    Break,
+    Chunked(Chunked),
+    Comment(String),
+    Dummy(Dummy),
+    Field(Field),
+    Array(Array),
+    Length(Length),
+    Switch(Switch),
+}
+
+#[derive(Debug, Deserialize)]
+struct Chunked {
+    #[serde(rename = "$value", default)]
+    pub elements: Vec<StructElement>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Field {
+    #[serde(rename = "@name")]
+    pub name: Option<String>,
+    #[serde(rename = "@type")]
+    pub data_type: String,
+    #[serde(rename = "$value", default)]
+    pub value: Option<String>,
+    pub comment: Option<String>,
+    #[serde(rename = "padded")]
+    pub padded: Option<bool>,
+    #[serde(rename = "optional")]
+    pub length: Option<String>,
+}
+
+fn default_as_true() -> bool {
+    true
+}
+
+#[derive(Debug, Deserialize)]
+struct Array {
+    #[serde(rename = "@name")]
+    pub name: String,
+    #[serde(rename = "@type")]
+    pub data_type: String,
+    #[serde(rename = "@length")]
+    pub length: Option<String>,
+    #[serde(rename = "optional")]
+    pub optional: Option<bool>,
+    #[serde(rename = "delimited")]
+    pub delimited: Option<bool>,
+    #[serde(rename = "@trailing-delimiter")]
+    #[serde(default = "default_as_true")]
+    pub trailing_delimiter: bool,
+    pub comment: Option<String>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Length {
+    #[serde(rename = "@name")]
+    pub name: String,
+    #[serde(rename = "@type")]
+    pub data_type: String,
+    #[serde(rename = "@length")]
+    pub optional: Option<bool>,
+    #[serde(rename = "@offset")]
+    pub offset: Option<i32>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Dummy {
+    #[serde(rename = "@type")]
+    pub data_type: String,
+    #[serde(rename = "$value", default)]
+    pub value: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct Switch {
+    #[serde(rename = "@field")]
+    pub field: String,
+    #[serde(rename = "$value", default)]
+    pub cases: Vec<Case>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename = "case")]
+struct Case {
+    #[serde(rename = "@default")]
+    pub default: Option<bool>,
+    #[serde(rename = "@value")]
+    pub value: Option<String>,
+    #[serde(rename = "$value", default)]
+    pub elements: Option<Vec<StructElement>>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Packet {
+    #[serde(rename = "@action")]
+    pub action: String,
+    #[serde(rename = "@family")]
+    pub family: String,
+    #[serde(rename = "$value", default)]
+    pub elements: Vec<StructElement>,
 }
 
 fn main() {
@@ -23,120 +177,18 @@ fn main() {
     }
 }
 
-enum ProtocolType {
-    Struct,
-    Enum,
-    Packet,
-}
-
 fn parse_protocol_file(path: &std::path::Path) -> std::io::Result<()> {
-    let file = File::open(path)?;
-    let file = BufReader::new(file);
+    let mut file = File::open(path)?;
+    let mut xml = String::new();
+    file.read_to_string(&mut xml)?;
 
-    let parser = EventReader::new(file);
-
-    let mut protocol_type: Option<ProtocolType> = None;
-    let mut protocol_enum: Option<Enum> = None;
-
-    for e in parser {
-        match e {
-            Ok(XmlEvent::StartElement {
-                name, attributes, ..
-            }) => {
-                p!("{}: {:?}", name, attributes);
-
-                match name.local_name.as_str() {
-                    "enum" => {
-                        let name = match attributes
-                            .iter()
-                            .find(|a| a.name.local_name == "name")
-                            .map(|a| a.name.local_name.to_owned())
-                        {
-                            Some(name) => name,
-                            None => {
-                                p!("Enum without name");
-                                continue;
-                            }
-                        };
-
-                        let r#type = match attributes
-                            .iter()
-                            .find(|a| a.name.local_name == "type")
-                            .map(|a| a.name.local_name.to_owned())
-                        {
-                            Some(r#type) => match r#type.as_str() {
-                                "byte" => EnumType::Byte,
-                                "char" => EnumType::Char,
-                                "short" => EnumType::Short,
-                                "three" => EnumType::Three,
-                                "int" => EnumType::Int,
-                                _ => {
-                                    p!("Unknown enum type: {}", r#type);
-                                    continue;
-                                }
-                            },
-                            None => {
-                                p!("Enum without type");
-                                continue;
-                            }
-                        };
-                        protocol_type = Some(ProtocolType::Enum);
-                        protocol_enum = Some(Enum::new(name, r#type));
-                    }
-                    _ => {}
-                }
-            }
-            Ok(XmlEvent::Characters(value)) => {
-                p!("Value: {}", value);
-            }
-            Err(e) => {
-                p!("Error: {}", e);
-                break;
-            }
-            _ => {}
+    let _protocol: Protocol = match quick_xml::de::from_str(&xml) {
+        Ok(protocol) => protocol,
+        Err(err) => {
+            p!("error: {} in {}", err, path.to_string_lossy());
+            return Ok(());
         }
-    }
+    };
+
     Ok(())
-}
-
-#[derive(Debug, Default)]
-struct Enum {
-    name: String,
-    r#type: EnumType,
-    fields: Vec<EnumField>,
-}
-
-impl Enum {
-    pub fn new(name: String, r#type: EnumType) -> Self {
-        Self {
-            name,
-            r#type,
-            fields: Vec::new(),
-        }
-    }
-
-    pub fn add_field(&mut self, name: String, value: u32) {
-        self.fields.push(EnumField { name, value });
-    }
-}
-
-#[derive(Debug)]
-enum EnumType {
-    Byte,
-    Char,
-    Short,
-    Three,
-    Int,
-}
-
-impl Default for EnumType {
-    fn default() -> Self {
-        Self::Byte
-    }
-}
-
-#[derive(Debug, Default)]
-struct EnumField {
-    pub name: String,
-    pub value: u32,
 }
